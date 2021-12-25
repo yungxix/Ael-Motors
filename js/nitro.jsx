af0e5d2e7be03626bd57d63671f4bfae4c3a8931
Folorunsho shina <58972665+yungxix@users.noreply.github.com>
2021-12-25 22:44:05 +0100

    Add files via upload

---
+++ js/nitro.jsx
@@ -0,0 +1,23 @@
+const users = [{
+    userName: "Michael_webster",
+    pass: 'identifier',
+    secret_code: 230
+},
+{
+userName: "Breand",
+pass: "louisana",
+secret_code: 434
+},
+{
+    userName: francis,
+    pass: "London",
+    secret_code: 404
+},
+{
+    userName: "Bryan",
+    pass: "Toronto",
+    secret_code: 500,
+    places:['ontario', "london", 'ferocx']
+}]
+
+  users.map()
